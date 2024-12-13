4ef4d96fedc6735e33b825c61baeffef0d5e43b3
Sergey Kubinski <kubinskisergey@gmail.com>
2024-12-13 18:44:46 +0200

    add file

--- Program.cs
+++ Program.cs
@@ -1,45 +1,96 @@
-﻿using System.Net.Security;
+﻿// 402/221
 
-namespace Course_Work_Exercise_7;
+namespace Course_Work_Exercise_6;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string [] cities = new string[50];
+        double [] rainings = new double[31];
+        int n = 0;
 
-        addCities(cities);
-
-        sortCities(cities);
-
-
-        
-    }
-    static void addCities (string [] cities)
-    {
-        for (int index = 0; index < cities.Length; index++)
+        while (true)
         {
-            Console.Write("Enter the name of the city (Exit - to stop adding): ");
-            string newCity = Console.ReadLine();
-            if (newCity.ToLower() == "exit")
+            Console.Write("Enter the month day count (<= 31): ");
+            int m = int.Parse(Console.ReadLine());
+            if (m > 0 && m <= 31)
             {
+                n = m;
                 break;
             }
-            else
-            {
-                newCity = char.ToUpper(newCity[0]) + newCity.Substring(1).ToLower();
-
-                cities[index] = newCity;
-            }
         }
+
+        double [] A = new double[n];
+        double [] B = new double[n];
+        double [] C = new double[n];
+
+        Console.WriteLine("Enter rain ammounts for station A: ");
+        addRains(A);
+
+        Console.WriteLine("Enter rain ammounts for station B: ");
+        addRains(B);
+
+        Console.WriteLine("Enter rain ammounts for station C: ");
+        addRains(C);
+
+        Console.WriteLine("Results: ");
+
+        PrintRainResults(A, "Station A");
+        PrintRainResults(B, "Station B");
+        PrintRainResults(C, "Station C");
+        
+
     }
 
-    static void sortCities (string [] cities)
+    static void addRains (double [] rainings)
     {
-        Array.Sort(cities);
-        for (int city = 0; city < cities.Length; city++)
+        for (int day = 0; day < rainings.Length; day++)
         {
-            Console.WriteLine(cities[city]);
+            Console.Write("Enter the ammount of rains in l/sq2 for day {0}: ", day + 1);
+            double ammount = double.Parse(Console.ReadLine());
+            rainings[day] = ammount;
+        }   
+    }
+
+    static double MinimalRains(double [] rainings)
+    {
+        double minRains = double.MaxValue; 
+        bool hasRain = false; 
+
+        foreach (double rains in rainings)
+        {
+            if (rains > 0) 
+            {
+                hasRain = true; 
+                if (rains < minRains) 
+                {
+                    minRains = rains;
+                }
+        }   }
+        return hasRain ? minRains : 0;
+    }
+
+    static void PrintRainResults(double[] rainings, string stationName)
+    {
+        double minRain = MinimalRains(rainings);
+
+        if (minRain == 0)
+        {
+            Console.WriteLine($"{stationName}: No days with rain.");
+        }
+        else
+        {
+            Console.WriteLine($"{stationName}: Minimum rain amount for rainy days is {minRain} l/sq2.");
+            Console.Write("Days with minimum rain: ");
+            for (int i = 0; i < rainings.Length; i++)
+            {
+                if (rainings[i] == minRain)
+                {
+                    Console.Write($"{i + 1} "); 
+                }
+            }
+            Console.WriteLine(); 
         }
     }
+
 }
